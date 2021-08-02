7c923bb83dcb2484498145dc1da0e37a14659ee6
AdiBarHalevi <adibar141@gmail.com>
2021-08-02 06:24:03 +0300

    server connected to the DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const MongoClient = require("mongodb").MongoClient;
 require('dotenv').config()
 
-
-const uri = process.env.DB_URI
+const publicAccess = "mongodb+srv://publicUser:publicUser@cluster0.irz1b.mongodb.net/POI-task?retryWrites=true&w=majority"
+const uri = process.env.DB_URI||publicAccess
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
